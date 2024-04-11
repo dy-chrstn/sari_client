3e401670ad3e7bad858996b0bf9016c04ede5996
Christian Andrei Dy <dy.cavd@gmail.com>
2024-04-11 16:24:50 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/sari/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sari
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Login());
+}
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Login'),
+        ),
+      ),
+    );
+  }
+}
