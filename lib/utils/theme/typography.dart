f1ef7c5bb0770ca6f09afae5552007fc05cb0efa
dy-chrstn <dy.cavd@gmail.com>
2024-05-11 16:17:51 +0800

    Typography configured

--- lib/utils/theme/typography.dart
+++ lib/utils/theme/typography.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:sari/utils/theme/colors.dart';
+import 'package:sari/utils/theme/constants.dart';
 
 class AppTypography {
   static const TextStyle heading1 = TextStyle(
     color: AppColors.primaryColor,
     fontSize: 24,
     fontWeight: FontWeight.bold,
+    fontFamily: AppConstants.fontFamily
   );
 
   static const TextStyle bodyText = TextStyle(
     color: AppColors.primaryColor,
+    fontFamily: AppConstants.fontFamily,
     fontSize: 16,
   );
 }
