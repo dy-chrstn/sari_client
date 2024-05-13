cf9804fa41ce21bba51940d19239dc5944b2ac17
dy-chrstn <dy.cavd@gmail.com>
2024-05-13 08:45:15 +0800

    Create pin page added

--- lib/account/views/profile/CreatePinPage.dart
+++ lib/account/views/profile/CreatePinPage.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:pinput/pinput.dart';
 import 'package:sari/utils/theme/colors.dart';
+
+import '../../../utils/theme/typography.dart';
+import '../../../widgets/form/textfield.dart';
 
 class CreatePin extends StatefulWidget {
   const CreatePin({super.key});
@@ -9,11 +14,126 @@ class CreatePin extends StatefulWidget {
 }
 
 class _CreatePinState extends State<CreatePin> {
+
+  TextEditingController pinController = TextEditingController();
+  late FocusNode pinFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    pinFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pinFocusNode.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    var screenSize = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: AppColors.backgroundColor,
-      body: Center(child: Text('Create Pin'),),
-    );
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: screenSize.height * 0.03,
+            ),
+            Center(
+              child: Text(
+                'Sari',
+                style: TextStyle(
+                    fontSize: screenSize.height * 0.06,
+                    fontWeight: FontWeight.w600,
+                    color: AppColors.textColor),
+              ),
+            ),
+            SizedBox(
+              height: screenSize.height * 0.01,
+            ),
+            SizedBox(
+              height: screenSize.height * 0.12,
+              width: screenSize.height * 0.12,
+              child: Card(
+                color: AppColors.primaryColor,
+                shape: CircleBorder(),
+                child: Icon(
+                  Icons.person,
+                  color: AppColors.dirtyWhite,
+                  size: screenSize.height * 0.06,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: screenSize.height * 0.03,
+            ),
+            Text('Create Pin', style: AppTypography.heading1),
+            SizedBox(
+              height: screenSize.height * 0.03,
+            ),
+            Pinput(
+              focusNode: pinFocusNode,
+              controller: pinController,
+              closeKeyboardWhenCompleted: true,
+              autofocus: true,
+              keyboardType: TextInputType.number,
+              obscureText: true,
+              defaultPinTheme: PinTheme(
+                width: 64,
+                height: 64,
+                textStyle: const TextStyle(
+                  fontSize: 20,
+                  color: AppColors.dirtyWhite,
+                  fontWeight: FontWeight.w600,
+                ),
+                margin: EdgeInsetsDirectional.all(screenSize.width * 0.01),
+                decoration: BoxDecoration(
+                  color: AppColors.primaryColor,
+                  borderRadius: BorderRadius.circular(32),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: screenSize.height * 0.10,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 55,
+              child: ElevatedButton(
+                  onPressed: () {
+                    // GoRouter.of(context).push('/profile/create/createPin');
+                  },
+                  style: AppForm.darkButton,
+                  child: const Text(
+                    'NEXT',
+                    style: TextStyle(color: AppColors.dirtyWhite),
+                  )),
+            ),
+            SizedBox(
+              height: screenSize.height * 0.02,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 55,
+              child: ElevatedButton(
+                  onPressed: () {
+                    GoRouter.of(context).pop();
+                  },
+                  style: AppForm.whiteButton,
+                  child: const Text(
+                    'CANCEL',
+                    style: TextStyle(color: AppColors.textColor),
+                  )),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
+
 }
